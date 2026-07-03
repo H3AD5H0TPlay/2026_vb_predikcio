b0bbaaeceefb2446e253fff898f7b7e4015499ac
H3AD5H0TPlay <vpeter993@gmail.com>
2026-07-03 18:14:23 +0200

    chore: remove debug scripts and revert debug export

--- test.js
+++
@@ -1,3 +0,0 @@
-const { DatabaseSync } = require('node:sqlite');
-const db = new DatabaseSync('prediction.db');
-console.log(db.prepare("SELECT * FROM matches WHERE stage IN ('r32', 'r16')").all());
